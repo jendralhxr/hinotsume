2cadadf477a7c467c454626e9c1b9776c9a16d8a
yoen <jendral.hxr@gmail.com>
2021-01-08 19:56:42 +0900

    ngantuk

--- vehicleselect.m
+++ vehicleselect.m
@@ -1,20 +1,19 @@
 STF=36000;
 
-threshold=3; # mm
+threshold=2.5; # mm
 displacement_thre(size(displacement,1), 7)=0; 
 #for j=1:size(displacement,1)
 for j=STF:size(displacement,1)
  for i=2:8
-  temp= displacement(j,i);
-  if (temp<threshold) && (temp>-threshold)
+  temp= abs(displacement(j,i));
+  if (temp<threshold)
    displacement_thre(j,i-1)= 0;
   else 
    displacement_thre(j,i-1)= abs(displacement(j,i));
-   #displacement_thre(j,i-1)= abs(displacement(j,i));
   endif
  endfor
 endfor
-plot(displacement_thre(:,4));
+plot(displacement_thre);
 
 #########
 
@@ -25,5 +24,3 @@ Some of setting is conducted by my python program.
 Additionally, you had better show all the photos when large displacements (e.g. >2mm). 
 Several left lane cars are misdetected as large displacement, but you should explain it with this. 
 
-did not indicate this paper is extension of your previous JRM paper. 
-Tandem-marker based motion capture realize very simple bridge observation.
