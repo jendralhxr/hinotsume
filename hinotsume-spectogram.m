ab9be7a6516f0ebad69c59d38c22b0e3d04a7fb0
yoen <jendral.hxr@gmail.com>
2021-05-17 14:55:30 +0900

    neater spectogram

--- hinotsume-spectogram.m
+++ hinotsume-spectogram.m
@@ -2,17 +2,18 @@ load("displacement.csv");
 
 window_width=60; # 1 s
 sample_width= 360; # 6 s
-jump=6;  # advances by .1 s
-duration_width= 18000; # last element, 5 min
+jump=2;  # advances by .1 s
+duration_width= 3600;
 
 for minut=0:1:12
 
 for markernum=1:7
-clear freqbar; freqbar(duration_width/jump, sample_width/2)=0;
+clear freqbar; 
+freqbar(duration_width/jump, sample_width/2)=0;
 
 step= 1;
 for i=1:jump:duration_width
-	window_sample= dtotal(i + minut*duration_width: i+window_width-1 + minut*duration_width, markernum);
+	window_sample= displacement(i + minut*duration_width: i+window_width-1 + minut*duration_width, markernum);
 	window_sample= window_sample .* hamming(window_width);
 	sample=0; sample(sample_width)=0;
 	sample(1:window_width)= window_sample;
@@ -21,20 +22,21 @@ for i=1:jump:duration_width
 endfor
 
 t=0: jump/60 : jump/60*(step-2);
+#t+=1080;
 freq=0:30/size(freqbar,2):29.9999;
-pcolor(t,freq,freqbar')
-shading flat
-axis([0 300 0 30])
-caxis([0 0.15]) # pick whatever appropriate
+pcolor(t,freq,freqbar');
+shading interp
+# axis([0 3600 0 30])
+caxis([0 0.3]) # pick whatever appropriate
 	
-xlabel("time (s)");
-ylabel("frequency (Hz)");
-zz=colorbar;
-ylabel(zz,"frequency response(mm)");
-title(sprintf("marker%d, t= %d - %d min",markernum, minut*5, (minut+1)*5));
+xlabel("Time [s]");
+ylabel("Frequency [Hz]");
+#zz=colorbar;
+#ylabel(zz,"Frequency Response [mm]");
+#title(sprintf("marker%d, t= %d - %d min",markernum, minut*5, (minut+1)*5));
 
 filename=sprintf("t%02dm%02d.png",minut,markernum);
-print(filename);
-
+#print(filename);
+print(filename, '-dpng', '-S1200,400');
 endfor
 endfor
